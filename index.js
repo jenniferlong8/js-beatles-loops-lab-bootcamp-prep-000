e64fb8bec53cfc876aac64ca1c7fe8ccb723c836
jenniferlong8 <jlong8@bu.edu>
2018-07-02 21:14:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// add solution here
+function theBeatlesPlay(musicians,instruments) {
+  var new_array = [];
+  for (let i=0; i<4; i++) {
+    new_array.push(`${musicians[i]} plays ${instruments[i]}`);
+  }
+    return new_array
+}
+
+function johnLennonFacts(array) {
+  var i = 0;
+  while (i < array.length) {
+    array[i] = array[i] + "!!!";
+    i++;
+  }
+    return array
+}
+
+function iLoveTheBeatles(n) {
+  
+}
