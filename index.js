8e07322b52594ed9e4d355082946311308a8680f
jenniferlong8 <jlong8@bu.edu>
2018-07-02 22:10:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,6 +20,6 @@ function iLoveTheBeatles(number) {
   do {
     array.push("I love the Beatles!");
     number++;
-  } while (number < 15);
+  } while (i <= number && number < 15);
     return array;
 }
